b5a3c8d700c447db30ed2d1e020706381e551d59
Miguel Vila <miviro0804@gmail.com>
2026-06-12 21:45:38 +0200

    fix nix-shell

--- shell.nix
+++ shell.nix
@@ -5,13 +5,13 @@ pkgs.mkShell {
   LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [
     pkgs.stdenv.cc.cc.lib  # libstdc++.so.6
     pkgs.zlib               # libz.so.1
-    pkgs.xorg.libxcb        # libxcb.so.1
+    pkgs.libxcb        # libxcb.so.1
     pkgs.libGL              # libGL.so.1
     pkgs.glib               # libglib-2.0.so.0, libgthread-2.0.so.0
-    pkgs.xorg.libXext       # libXext.so.6
-    pkgs.xorg.libX11        # libX11.so.6
-    pkgs.xorg.libSM         # libSM.so.6
-    pkgs.xorg.libICE        # libICE.so.6
+    pkgs.libxext       # libXext.so.6
+    pkgs.libx11        # libX11.so.6
+    pkgs.libsm         # libSM.so.6
+    pkgs.libice        # libICE.so.6
     pkgs.cudaPackages.cudatoolkit        # libcudart.so, libcublas.so, etc.
     pkgs.cudaPackages.cudatoolkit.lib
   ]}:/run/opengl-driver/lib";  # libcuda.so from the NVIDIA driver
